728c28aa1682dd4260b85ec0d2a4ee2b99f893c3
Nasiru <nasiruadamu214@gmail.com>
2025-04-01 08:07:23 +0100

    Remove unused import from homepage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <h1 className="text-5xl  text-black pt-4 text-center"> Hello, world</h1>
+    <h1 className="text-5xl text-black pt-4 text-center"> Hello, world</h1>
   );
 }
