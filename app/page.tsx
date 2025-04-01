783524d3e41571de5dd489bb655d4c6297a899d4
Nasiru <nasiruadamu214@gmail.com>
2025-04-01 06:37:13 +0100

    Enhance homepage greeting: increase font size, change text color to black, and add padding

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="text-3xl text-amber-300 text-center"> hello, world</h1>;
+  return (
+    <h1 className="text-5xl  text-black pt-4 text-center"> Hello, world</h1>
+  );
 }
