45a3fe8c64270dfb07982ee50b9c09e3064410c9
Nasiru <nasiruadamu214@gmail.com>
2025-04-02 20:15:36 +0100

    Add various asset files and remove unused vercel.svg

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,43 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import PatientForm from "@/components/forms/PatientForm";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-white underline">Home</h1>
-      <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen">
+      {/* otp verification */}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/images/thera_logo.png"
+            alt="patient"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10  w-fit"
+          />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2025 TheraFlow
+            </p>
+
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding.jpg"
+        alt="patient"
+        width={1000}
+        height={1000}
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
