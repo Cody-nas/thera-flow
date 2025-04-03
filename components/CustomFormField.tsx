42db65079f856dabc37128747f2bb911c9448f48
Nasiru <nasiruadamu214@gmail.com>
2025-04-03 10:26:24 +0100

    Remove unused SVG icons and add new user and email icons; update PatientForm and CustomFormField components for improved form handling

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -9,24 +9,76 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Control } from "react-hook-form";
+import { FormFieldtype } from "@/components/forms/PatientForm";
+import Image from "next/image";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
 
 interface CustomProps {
   control: Control<any>;
+  fieldType: FormFieldtype;
+  name: string;
+  label: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
 }
 
-const CustomFormField = ({ control }: CustomProps) => {
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  const { fieldType, iconSrc, iconAlt, placeholder } = props;
+  switch (fieldType) {
+    case FormFieldtype.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400 ">
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              height={24}
+              width={24}
+              alt={iconAlt || "icon"}
+              className="ml-2 "
+            />
+          )}
+
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              {...field}
+              className="shad-input border-0"
+            />
+          </FormControl>
+        </div>
+      );
+    case FormFieldtype.PHONE_INPUT:
+      return (
+        <FormControl>
+          <PhoneInput defaultCountry="NG" />
+        </FormControl>
+      );
+    default:
+      break;
+  }
+};
+
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, name, label } = props;
   return (
     <FormField
       control={control}
-      name="username"
+      name="name"
       render={({ field }) => (
-        <FormItem>
-          <FormLabel>Username</FormLabel>
-          <FormControl>
-            <Input placeholder="shadcn" {...field} />
-          </FormControl>
-          <FormDescription>This is your public display name.</FormDescription>
-          <FormMessage />
+        <FormItem className="flex-1">
+          {fieldType !== FormFieldtype.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
+
+          <RenderField field={field} props={props} />
+
+          <FormMessage className="shad-error" />
         </FormItem>
       )}
     />
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -5,8 +5,18 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
-
 import CustomFormField from "@/components/CustomFormField";
+import { FaUser } from "react-icons/fa";
+
+export enum FormFieldtype {
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datepicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
+}
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -38,7 +48,33 @@ const PatientForm = () => {
           <p className="text-dark-700">Schedule your first appointment.</p>
         </section>
 
-        <CustomFormField control={form.control} />
+        <CustomFormField
+          fieldType={FormFieldtype.INPUT}
+          control={form.control}
+          name="name"
+          label="Full Name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldtype.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="johndoe@gmail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="user"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldtype.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone Number"
+          placeholder="(+234) 123-4567"
+        />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
