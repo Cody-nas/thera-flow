0a4b1b117bd3612861938b1eb354866209ee8d89
Cody-nas <174928953+Cody-nas@users.noreply.github.com>
2025-05-01 07:41:53 +0100

    feat: update AppointmentForm to use getAppointmentSchema and implement appointment creation logic

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -7,13 +7,13 @@ import { Form } from "@/components/ui/form";
 import CustomFormField from "@/components/CustomFormField";
 import SubmitButton from "../ui/SubmitButton";
 import { useState } from "react";
-import { AppointmentFormvalidation } from "@/lib/validation";
+import { getAppointmentSchema } from "@/lib/validation";
 import { useRouter } from "next/navigation";
-import { createUser } from "@/lib/actions/patient.action";
 import { FormFieldtype } from "./PatientForm";
 import { Doctors } from "@/constants";
 import { SelectItem } from "../ui/select";
 import Image from "next/image";
+import { createAppointment } from "@/lib/actions/appointment.actions";
 
 const AppointmentForm = ({
   userId,
@@ -26,6 +26,8 @@ const AppointmentForm = ({
 }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
+
+  const AppointmentFormvalidation = getAppointmentSchema(type);
 
   const form = useForm<z.infer<typeof AppointmentFormvalidation>>({
     resolver: zodResolver(AppointmentFormvalidation),
@@ -62,12 +64,19 @@ const AppointmentForm = ({
           patient: patientId,
           primaryPhysician: values.primaryPhysician,
           schedule: new Date(values.schedule),
-          reason: values.reason,
+          reason: values.reason!,
           note: values.note,
           status: status as Status,
         };
+        const appointment = await createAppointment(appointmentData);
+
+        if (appointment) {
+          form.reset();
+          router.push(
+            `/patients/${userId}/new-appointment/success?appointmentId=${appointment.id}`
+          );
+        }
       }
-      const appointment = await CreateAppointment(appointmentData);
     } catch (error) {
       console.log(error);
     }
